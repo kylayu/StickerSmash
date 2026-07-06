b0c500c6f41b12701942377a899e3f20be697d55
Arvi002 <mark.arvi15@gmail.com>
2026-07-06 14:20:55 +0800

    add first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Kyla Mae Ypon, 3A</Text>
+      <Text>Mobile App Development</Text>
+    </View>
+  );
+}
